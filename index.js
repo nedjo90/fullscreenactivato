1a3a3e5693db5ca2c9b0872f3ecab3600a78a907
nedjo90 <han.necati@gmail.com>
2025-03-10 11:26:36 +0100

    update

--- index.js
+++ index.js
@@ -1,53 +1,35 @@
-let isFullScreen = false;
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+var track = null;
 
-toggleFullscreenButton = document.getElementById("toggleFullscreenButton");
-targetFullScreen = document.getElementById("item");
-console.log("targetFullScreen :>> ", targetFullScreen);
-toggleFullscreenButton.addEventListener("click", () => {
-  if (!isFullScreen) {
-    targetFullScreen.classList.add("fullscreen");
-    document.documentElement.requestFullscreen();
-  } else {
-    targetFullScreen.classList.remove("fullscreen");
-    document.exitFullscreen();
-  }
-  isFullScreen = !isFullScreen;
-});
-const video = document.getElementById("camera");
-const quitButton = document.getElementById("quit");
-toggleCameraButton = document.getElementById("toggleCameraButton");
-toggleCameraButton.addEventListener("click", (e) => {
-  navigator.mediaDevices
-    .getUserMedia({
-      video: {
-        width: {
-          min: screen.width,
-        },
-        height: {
-          min: screen.height,
-        },
-      },
-    })
-    .then((stream) => {
-      console.log(screen.width);
-      console.log(screen.height);
-      video.srcObject = stream;
-      video.style.visibility = "visible";
-      quitButton.style.visibility = "visible";
-      document.documentElement.requestFullscreen();
-    })
-    .catch((error) => {
-      console.log("error accessing the camera", error);
-    });
-});
+// Define constants
+const cameraView = document.querySelector("#camera--view"),
+    cameraOutput = document.querySelector("#camera--output"),
+    cameraSensor = document.querySelector("#camera--sensor"),
+    cameraTrigger = document.querySelector("#camera--trigger");
 
-// Gérer l'arrêt de la caméra avec "Quit"
-quitButton.addEventListener("click", () => {
-  const stream = video.srcObject;
-  const tracks = stream.getTracks();
-  tracks.forEach((track) => track.stop()); // Arrêter la caméra
-  video.srcObject = null;
-  video.style.visibility = "hidden";
-  quitButton.style.visibility = "hidden"; // Cacher le bouton
-  document.exitFullscreen();
-});
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+            track = stream.getTracks()[0];
+            cameraView.srcObject = stream;
+        })
+        .catch(function(error) {
+            console.error("Oops. Something is broken.", error);
+        });
+}
+
+// Take a picture when cameraTrigger is tapped
+cameraTrigger.onclick = function() {
+    cameraSensor.width = cameraView.videoWidth;
+    cameraSensor.height = cameraView.videoHeight;
+    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    cameraOutput.src = cameraSensor.toDataURL("image/webp");
+    cameraOutput.classList.add("taken");
+    // track.stop();
+};
+
+// Start the video stream when the window loads
+window.addEventListener("load", cameraStart, false);
